2df0e78a8e360aee68713556f85010bb83733286
symi <31873316+symienMoore@users.noreply.github.com>
2025-09-11 01:55:09 -0400

    got forms going for login

--- ui/src/app/pages/home/home.ts
+++ ui/src/app/pages/home/home.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+
+    // login: login 
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
+
+
+  doUserLogin() {
+    console.log(this.loginForm.value);
+  }
 
 }
---
+++ ui/src/app/types/login.d.ts
@@ -0,0 +1,4 @@
+type login = {
+    email: string;
+    password: string;
+}
